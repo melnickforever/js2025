0154a442d754131e27feede97cc98d1b159e74c5
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-01-14 15:19:03 +0200

    JavaScript Fundamentals – Part 1. code challenge4

--- 01-Fundamentals-Part-1/final/script.mjs
+++ 01-Fundamentals-Part-1/final/script.mjs
@@ -1,2 +1,6 @@
-let mName = "Dima";
-console.log(`Hello ${mName} you are ${12+12}`);
+const bill = 10;
+
+/* Write your code below. Good luck! 🙂 */
+const tip = (bill >= 50 && bill <= 300) ? bill*15/100 : bill*20/100;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill+tip}`)
