c1c7d1aa59fc3098bdd1824a110261d8fe69b297
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-01-15 09:02:28 +0200

    JavaScript Fundamentals – Part 2. strict mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('I can drive');
+
+//const interface = 'Audio';
+//const private = 345; // private is a reserved word
+//const if = 23; // if is a reserved word
