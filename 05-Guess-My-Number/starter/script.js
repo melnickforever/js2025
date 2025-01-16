a4aab4e38d6d8256e9444e29a1e561ad25852e10
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-01-16 15:50:19 +0200

    Guess my number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,7 @@
 'use strict';
+document.querySelector('.message').textContent = 'correct number';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
