3f1d2126347249a78e80e928f3f43655480796a9
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-01-16 12:09:48 +0200

    JavaScript Fundamentals – Part 2. Loops

--- 02-Fundamentals-Part-2/starter/script.mjs
+++ 02-Fundamentals-Part-2/starter/script.mjs
@@ -4,3 +4,26 @@
 for (let rep = 1; rep <= 10; rep++) {
     console.log(`Lifting weights repetition ${rep}`);
 }
+
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michale', 'Peter', 'Steven'],
+    true
+];
+
+for (let i = 0; i < jonasArray.length; i++) {
+    console.log(jonasArray[i], typeof jonasArray[i]);
+}
+
+for (let i = jonasArray.length - 1; i >= 0; i--) {
+    console.log(jonasArray[i]);
+}
+
+let rep = 1;
+while (rep < 10) {
+    console.log(`Lifting weights repetition ${rep}`);
+    rep++;
+}
