2d0a5c487a338e1b955615ce2f48f8fa69796687
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-01-16 11:53:11 +0200

    JavaScript Fundamentals – Part 2. Loops

--- 02-Fundamentals-Part-2/starter/script.mjs
+++ 02-Fundamentals-Part-2/starter/script.mjs
@@ -1,35 +1,6 @@
 /* Write your code below. Good luck! 🙂 */
 'use strict';
 
-const jonas = {
-  firstName: 'Jonas',
-  lastName: 'Schmedtmann',
-  birthYear: 1991,
-  job: 'teacher',
-  friends: ['Michael', 'Peter', 'Steven'],
-  hasDriversLicense: true,
-
-  // calcAge: function(birthYear) {
-  //     return  2037 - birthYear;
-  // }
-
-    calcAge: function() {
-        if (!this.age) {
-            this.age = 2037 - this.birthYear;
-        }
-        return this.age;
-    },
-
-    getSummary: function () {
-      return `Summary: ${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`;
-    }
-
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
 }
-
-console.log(jonas.calcAge());
-console.log(jonas['calcAge']());
-
-// Challenge
-// "Jonas has 3 friends, and his best friend is called Michael"
-console.log(jonas.getSummary());
-
