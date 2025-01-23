9e21c433ab6e3a94f7cdfe34df5cdeafa93c16cb
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-01-23 16:30:42 +0200

    Behind the scenes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,32 +1,29 @@
 'use strict';
-console.log(this); // Window
 
-const calcAge = function (birthYear) {
-    console.log(2037 - birthYear);
-    console.log(this); // undefined
-}
-calcAge(1991);
-
-const calcAgeArrow = birthYear => {
-    console.log(2037 - birthYear);
-    console.log(this); // undefined
-}
-calcAgeArrow(1980);
+var firstName = 'Matilda';
 
 const jonas = {
+    firstName: 'Jonas',
     year: 1991,
     calcAge: function () {
-        console.log(this);
+        //console.log(this);
         console.log(2037 - this.year);
-    }
+
+        // const self = this; // self or that
+        // const isMillenial = function () {
+        //     console.log(self);
+        //     console.log(self.year >= 1981 && self.year <= 1996);
+        // }
+
+        const isMillenial = () => {
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <= 1996);
+        }
+
+        isMillenial();
+    },
+
+    greet: () => console.log(`Hey ${this.firstName}`), // this is undefined
 }
+jonas.greet();
 jonas.calcAge();
-
-const matilda = {
-    year: 2017,
-}
-matilda.calcAge = jonas.calcAge;
-matilda.calcAge();
-
-const f = jonas.calcAge;
-f();
