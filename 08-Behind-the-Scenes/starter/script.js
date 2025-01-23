8b4f2bed1e9de5539e6d6a0632e63af3874fc1ab
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-01-23 15:41:39 +0200

    Behind the scenes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,34 +1,33 @@
-'use strict';
+console.log(me);
+//console.log(job);
+//console.log(year);
 
-function calcAge(birthYear) {
-    const age = 2037 - birthYear;
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
 
-    function printAge() {
-        let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-        console.log(output);
+// Functions
+console.log(addDecl(2, 3));
+//console.log(addExpr(2, 3));
+//console.log(addArrow(2, 3));
 
-        if (birthYear >= 1981 && birthYear <= 1996) {
-            var millenial = true;
-            const firstName = 'Steven';
-            const str = `Oh, and you're a millenial, ${firstName}`;
-            console.log(str);
-
-            function add(a, b) {
-                return a + b;
-            }
-
-            output = 'New Output';
-        }
-        console.log(millenial);
-        //add(2, 3);
-        console.log(output);
-    }
-
-    printAge();
-
-    return age;
+function addDecl(a, b) {
+    return a + b;
 }
 
-const firstName = 'Jonas';
-calcAge(1991);
+var addExpr = function (a, b) {
+    return a + b;
+};
 
+var addArrow = (a, b) => a + b;
+
+// Example
+if (!numProducts) {
+    deleteShoppingCart();
+}
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+    console.log('All products deldeted!');
+}
