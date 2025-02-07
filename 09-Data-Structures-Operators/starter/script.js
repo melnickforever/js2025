8a76236e479c73c611e90a3be1881efe7ad82a8b
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-02-07 14:34:23 +0200

    Data structures: maps

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,37 +1,65 @@
 'use strict';
 
-const italianFoods = new Set([
-                                 'pasta',
-                                 'gnocchi',
-                                 'tomatoes',
-                                 'olive oil',
-                                 'garlic',
-                                 'basil',
-                             ]);
+// Selecting elements
 
-const mexicanFoods = new Set([
-                                 'tortillas',
-                                 'beans',
-                                 'rice',
-                                 'tomatoes',
-                                 'avocado',
-                                 'garlic',
-                             ]);
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
 
-const commonFoods = mexicanFoods.intersection(italianFoods);
-//const commonFoods = new Set([...italianFoods].filter(food => mexicanFoods.has(food)));
+rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+    .set('open', 11)
+    .set('close', 23)
+    .set(true, 'We are open :D')
+    .set(false, "We closed :(");
 
-const italianMexicanFoods = new Set([...italianFoods, ...mexicanFoods]);
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
 
-console.log(commonFoods);
-console.log([...commonFoods]);
-console.log(italianMexicanFoods);
+const time = 6;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
 
-const unigueItalianFoods = italianFoods.difference(mexicanFoods);
-console.log(unigueItalianFoods);
+console.log(rest.has('categories'));
+rest.delete(2);
+const arr = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest);
+console.log(rest.size);
+//rest.clear();
+console.log(rest.get(arr));
 
-const unigueMexicanFoods = mexicanFoods.difference(italianFoods);
-console.log(unigueMexicanFoods);
+const question = new Map([
+                             ['question', 'What is the best programming language in the world?'],
+                             [1, 'C'],
+                             [2, 'Java'],
+                             [3, 'Javasript'],
+                             ['correct', 3],
+                             [true, 'Correct!'],
+                             [false, 'Try again!'],
+                         ]);
 
-const uniqueItalianMexicanFoods = italianFoods.symmetricDifference(mexicanFoods);
-console.log(uniqueItalianMexicanFoods);
+console.log(question);
+
+const hours = {
+    open: 11,
+    close: 23,
+};
+// Convert object to map
+console.log(Object.entries(hours));
+const hoursMap = new Map(Object.entries({open: 11, close: 23}));
+console.log(hoursMap);
+
+for (const [key, val] of question) {
+    if (typeof key === 'number') {
+        console.log(`Answer ${key}: ${val}`);
+    }
+}
+
+// const answer = Number(prompt('Your answer'));
+//
+// console.log(question.get('correct') === answer ? console.log(question.get(true)) : console.log(question.get(false)));
+
+// Convert map to array
+console.log([...question]);
+console.log([...question.entries()]);
