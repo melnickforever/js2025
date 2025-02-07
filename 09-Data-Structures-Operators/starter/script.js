bb8674266c56aec05636dd80b8542cb7a204d39e
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-02-07 09:56:24 +0200

    Data structures: for of

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,61 +1,72 @@
 'use strict';
 
-const game = {
-    team1: 'Bayern Munich',
-    team2: 'Borrussia Dortmund',
-    players: [
-        [
-            'Neuer',
-            'Pavard',
-            'Martinez',
-            'Alaba',
-            'Davies',
-            'Kimmich',
-            'Goretzka',
-            'Coman',
-            'Muller',
-            'Gnarby',
-            'Lewandowski',
-        ],
-        [
-            'Burki',
-            'Schulz',
-            'Hummels',
-            'Akanji',
-            'Hakimi',
-            'Weigl',
-            'Witsel',
-            'Hazard',
-            'Brandt',
-            'Sancho',
-            'Gotze',
-        ],
-    ],
-    score: '4:0',
-    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-    date: 'Nov 9th, 2037',
-    odds: {
-        team1: 1.33,
-        x: 3.25,
-        team2: 6.5,
+// Data needed for a later exercise
+const flights =
+    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const italianFoods = new Set([
+                                 'pasta',
+                                 'gnocchi',
+                                 'tomatoes',
+                                 'olive oil',
+                                 'garlic',
+                                 'basil',
+                             ]);
+
+const mexicanFoods = new Set([
+                                 'tortillas',
+                                 'beans',
+                                 'rice',
+                                 'tomatoes',
+                                 'avocado',
+                                 'garlic',
+                             ]);
+
+// Data needed for first part of the section
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
+        console.log(
+            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} Will be delivered to ${address} at ${time}`
+        );
+    },
+
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
     },
 };
 
-const [palyers1, players2] = game.players;
-console.log(palyers1, players2);
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
-const [gk, ...fieldPlayers] = palyers1;
-console.log(gk, fieldPlayers);
+for (const item of menu) {
+    console.log(item);
+}
 
-const allplayers = [...palyers1, ...players2];
-console.log(allplayers);
-
-const players1Final = [...palyers1, 'Thiago', 'Coutinho', 'Perisic'];
-console.log(players1Final);
-
-const {team1, x: draw, team2} = game.odds;
-console.log(team1, draw, team2);
-
-function printGoals(...players) {
-    consolo.log(`${players.length} goals were scored`);
+for (const [i, item] of menu.entries()) {
+    console.log(i, item);
 }
