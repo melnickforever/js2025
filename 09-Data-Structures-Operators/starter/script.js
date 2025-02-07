c9a66135e648ab707f5a44311632cb6727c6306b
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-02-07 13:59:19 +0200

    Data structures: sets new methods.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,66 +1,37 @@
 'use strict';
-const week = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-const openingHours = {
-    [`d-week[3]`]: {
-        open: 12,
-        close: 22,
-    },
-    fri: {
-        open: 11,
-        close: 23,
-    },
-    sat: {
-        open: 0, // Open 24 hours
-        close: 24,
-    },
-};
 
-const restaurant = {
-    name: 'Classico Italiano',
-    location: 'Via Angelo Tavanti 23, Firenze, Italy',
-    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+const italianFoods = new Set([
+                                 'pasta',
+                                 'gnocchi',
+                                 'tomatoes',
+                                 'olive oil',
+                                 'garlic',
+                                 'basil',
+                             ]);
 
-    openingHours,
+const mexicanFoods = new Set([
+                                 'tortillas',
+                                 'beans',
+                                 'rice',
+                                 'tomatoes',
+                                 'avocado',
+                                 'garlic',
+                             ]);
 
-    order(starterIndex, mainIndex) {
-        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-    },
+const commonFoods = mexicanFoods.intersection(italianFoods);
+//const commonFoods = new Set([...italianFoods].filter(food => mexicanFoods.has(food)));
 
-    orderDelivery({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
-        console.log(
-            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} Will be delivered to ${address} at ${time}`
-        );
-    },
+const italianMexicanFoods = new Set([...italianFoods, ...mexicanFoods]);
 
-    orderPizza(mainIngredient, ...otherIngredients) {
-        console.log(mainIngredient);
-        console.log(otherIngredients);
-    },
+console.log(commonFoods);
+console.log([...commonFoods]);
+console.log(italianMexicanFoods);
 
-};
+const unigueItalianFoods = italianFoods.difference(mexicanFoods);
+console.log(unigueItalianFoods);
 
-// console.log(restaurant);
-//
-// console.log(restaurant.openingHours.mon?.open);
+const unigueMexicanFoods = mexicanFoods.difference(italianFoods);
+console.log(unigueMexicanFoods);
 
-const orderSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza']);
-orderSet.add('Garlic Bread');
-orderSet.add('Garlic Bread');
-orderSet.delete('Risotto');
-console.log(orderSet);
-console.log(orderSet.size);
-console.log(orderSet);
-
-for (const order of orderSet) {
-    console.log(order);
-}
-
-const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
-const staffUnique = new Set(staff);
-console.log(staffUnique);
-
-const staffUniqueArray = [...new Set(staff)];
-console.log(staffUniqueArray);
-console.log(new Set('dima').size);
+const uniqueItalianMexicanFoods = italianFoods.symmetricDifference(mexicanFoods);
+console.log(uniqueItalianMexicanFoods);
