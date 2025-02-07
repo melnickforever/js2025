2c77a3e1158f25eb8213fbf31d7d51671d97c38e
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-02-07 11:49:56 +0200

    Data structures: code

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -41,6 +41,53 @@ const restaurant = {
 
 };
 
-console.log(restaurant);
+// console.log(restaurant);
+//
+// console.log(restaurant.openingHours.mon?.open);
 
-console.log(restaurant.openingHours.mon?.open);
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+};
+
+const scoredPlayers = game.scored;
+
+for (const [goal, player] of scoredPlayers.entries()) {
+    console.log(`Goal ${goal + 1}: ${player}`);
+}
