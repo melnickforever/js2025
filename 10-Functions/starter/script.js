af991f54e6fc9c2729a40590c25c0cbca635f327
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-02-20 11:08:48 +0200

    Functions: function returns function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,27 +1,15 @@
 'use strict';
 
-const oneWord = function (str) {
-    return str.replace(/ /g, '').toLowerCase();
-}
+// const greet = function (greeting) {
+//     return function (name) {
+//         console.log(`${greeting} ${name}`);
+//     }
+// }
 
-const upperFirstWord = function (str) {
-    const [first, ...others] = str.split(' ');
-    return [first.toLocaleUpperCase(), ...others].join()
-}
+const greet = (greeting) => (name) => console.log(`${greeting} ${name}`);
 
-const trasformer = function (str, fn) {
-    console.log(`Original string: ${str}`);
-    console.log(`Transformed string: ${fn(str)}`);
-    console.log(`Transformed by: ${fn.name}`);
-}
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
 
-trasformer('Javasvript is the best!', upperFirstWord);
-
-trasformer('Javasvript is the best!', oneWord);
-
-const high5 = function () {
-    console.log('👋');
-}
-document.body.addEventListener('click', high5);
-
-['Jonas', 'Martha', 'Adam'].forEach(high5);
+greet("Hello")('Jonas');
