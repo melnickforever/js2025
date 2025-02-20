32f4eef9a333c73f272e29e1e62e35bcca8305ce
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-02-20 07:03:28 +0200

    Data structures:

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,24 +1,6 @@
 'use strict';
 
-const gameEvents = new Map([
-                               [17, '⚽️ GOAL'],
-                               [36, '🔁 Substitution'],
-                               [47, '⚽️ GOAL'],
-                               [61, '🔁 Substitution'],
-                               [64, '🔶 Yellow card'],
-                               [69, '🔴 Red card'],
-                               [70, '🔁 Substitution'],
-                               [72, '🔁 Substitution'],
-                               [76, '⚽️ GOAL'],
-                               [80, '⚽️ GOAL'],
-                               [92, '🔶 Yellow card'],
-                           ]);
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
 
-gameEvents.delete(64);
-const gameEventSet = new Set(gameEvents.values());
-const events = [...gameEventSet];
-
-gameEvents.forEach((value, key) => {
-    console.log(`[${key < 45 ? 'FIRST HALF' : 'SECOND HALF'}] ${key}: ${value}`);
-});
-console.log(events);
+console.log(plane[0]);
