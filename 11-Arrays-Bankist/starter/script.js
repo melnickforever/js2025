a4faf152b0cf1ea775c31f5fa8aec5c2b80efb25
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-02-27 11:53:34 +0200

    Arrays. Methods at

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -75,33 +75,10 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
 
-// Slice
-console.log(arr.slice(2, 4)); // ['c', 'd']
-console.log(arr.slice(2)); // ['c', 'd', 'e']
-console.log(arr.slice(-2)); // ['d', 'e']
-console.log(arr.slice(-1)); // ['e']
-console.log(arr.slice(1, -2));
-console.log(arr.slice());
-console.log([...arr]);
-
-// SPLICE
-//console.log(arr.splice(2)); // ['c', 'd', 'e']
-arr.slice(-1);
-arr.slice(1, 2);
-console.log(arr);
-
-// REVERSE
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
-
-// CONCAT
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
-
-//Join
-console.log(letters.join(' - '));
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
