582060d023befaa541d52ac676b8dd11b970b52d
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-02-27 11:48:36 +0200

    Arrays. Methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,34 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// Slice
+console.log(arr.slice(2, 4)); // ['c', 'd']
+console.log(arr.slice(2)); // ['c', 'd', 'e']
+console.log(arr.slice(-2)); // ['d', 'e']
+console.log(arr.slice(-1)); // ['e']
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+// SPLICE
+//console.log(arr.splice(2)); // ['c', 'd', 'e']
+arr.slice(-1);
+arr.slice(1, 2);
+console.log(arr);
+
+// REVERSE
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+//Join
+console.log(letters.join(' - '));
