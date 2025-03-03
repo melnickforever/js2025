47c650c2cfea71a06013e04adbb7a3e3ffa1e28f
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-03-03 07:46:40 +0200

    Arrays. code

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -84,3 +84,11 @@ const calcAverageHumanAge = function (ages) {
     console.log(average);
 }
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+
+const modernAverage = function (ages) {
+    const modernAverage = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+        .filter(age => age >= 18)
+        .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+    console.log(modernAverage);
+}
+modernAverage([5, 2, 4, 1, 15, 8, 3]);
