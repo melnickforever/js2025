b793fcf031805ed6a565652198e23769befd4c56
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-05-06 12:38:45 +0300

    Dom events

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -33,3 +33,23 @@ document.addEventListener('keydown', function (e) {
 const logo = document.querySelector('.nav__logo');
 const nav = document.querySelector('.nav');
 logo.getAttribute('src');
+
+const h1 = document.querySelector('h1');
+// h1.addEventListener('mouseenter', function (e) {
+//     alert("addEventListener: Great! You are reading the heading");
+// });
+
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const randomCalor = () => `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+    this.style.backgroundColor = randomCalor();
+    e.preventDefault();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+    this.style.backgroundColor = randomCalor();
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+    console.log('link');
+});
