3feffa367cad943d6daa1e75f1193fea5a0cc065
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-05-16 10:41:01 +0300

    Dom events

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -3,53 +3,15 @@
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
-const openModal = function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-};
-
-const closeModal = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-};
-
-for (let i = 0; i < btnsOpenModal.length; i++) {
-    btnsOpenModal[i].addEventListener('click', openModal);
-}
-
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-        closeModal();
-    }
-});
-const logo = document.querySelector('.nav__logo');
-const nav = document.querySelector('.nav');
-logo.getAttribute('src');
-
 const h1 = document.querySelector('h1');
-// h1.addEventListener('mouseenter', function (e) {
-//     alert("addEventListener: Great! You are reading the heading");
-// });
+// Selecting elements: child
+console.log(h1.document.querySelector('.highlight'))
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'ortangered';
 
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-const randomCalor = () => `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
-document.querySelector('.nav__link').addEventListener('click', function (e) {
-    this.style.backgroundColor = randomCalor();
-    e.preventDefault();
-});
-
-document.querySelector('.nav__links').addEventListener('click', function (e) {
-    this.style.backgroundColor = randomCalor();
-});
-
-document.querySelector('.nav').addEventListener('click', function (e) {
-    console.log('link');
-});
+// Selecting elements: parent
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+console.log(h1.closest('.header')); 
