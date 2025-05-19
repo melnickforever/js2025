be9b31a9eff3d70e2264addcd723dbf87e9480f9
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-05-19 16:11:16 +0300

    oop: prototype

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -22,3 +22,20 @@ const matilda = new Person('Matilda', 2017);
 const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 console.log(jonas instanceof Person);
+
+// Prototypes
+Person.prototype.calcAge = function () {
+    console.log(2037 - this.birthYear);
+}
+
+jonas.calcAge();
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+
+// .prototypeOfLinkedObjects
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
