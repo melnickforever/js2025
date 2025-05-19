59d98833277f1ddc5b7077943a78ac4e615e74f5
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-05-19 15:55:38 +0300

    oop: basics

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+    console.log(this);
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // Never do this!
+    // this.calcAge = function () {
+    //     console.log(2037 - this.birthYear);
+    // }
+}
+const jonas = new Person("Jonas", 1991);
+console.log(jonas);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} is linked to prototype
+// 4. function automatically returns {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+console.log(jonas instanceof Person);
