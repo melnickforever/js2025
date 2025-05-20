5a757392c45962145faedd40d3ffc7f2e426fc83
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-05-20 17:01:25 +0300

    oop: object. private

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,140 +1,26 @@
-const Person = function (firstName, birthYear) {
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-}
+class Account {
 
-Person.prototype.calcAge = function () {
-    console.log(2037 - this.birthYear);
-}
+    locale = navigator.language;
+    bank = 'Bankist';
+    #movements = [2];
+    #pin;
 
-const Student = function (firstName, birthYear, course) {
-    Person.call(this, firstName, birthYear);
-    this.course = course;
-}
-
-Student.prototype = Object.create(Person.prototype);
-
-Student.prototype.introduce = function () {
-    console.log(`My name is ${this.firstName} and I study ${this.course}`);
-}
-const mike = new Student('Mike', 2020, 'CS');
-mike.introduce();
-mike.calcAge();
-console.log(mike);
-
-const Car = function (make, speed) {
-    this.make = make;
-    this.speed = speed;
-}
-
-Car.prototype.accelerate = function () {
-    this.speed += 10;
-    console.log(`${this.make} is going at ${this.speed} km/h`);
-}
-
-Car.prototype.broke = function () {
-    this.speed -= 5;
-    console.log(`${this.make} is going at ${this.speed} km/h`);
-}
-
-const EV = function (make, speed, charge) {
-    Car.call(this, make, speed);
-    this.charge = charge;
-}
-
-EV.prototype = Object.create(Car.prototype);
-
-EV.prototype.chargeBattery = function (chargeTo) {
-    this.charge = chargeTo;
-}
-
-EV.prototype.accelerate = function () {
-    this.speed += 20;
-    this.charge--;
-    console.log(`${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}`);
-
-}
-
-const tesla = new EV('Tesla', 120, 23);
-tesla.chargeBattery(90);
-console.log(tesla);
-tesla.broke();
-tesla.accelerate();
-tesla.accelerate();
-
-class PersonCL {
-    constructor(fullName, birthYear) {
-        this.fullName = fullName;
-        this.birthYear = birthYear;
+    constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.interestRate = 1.5;
+        this.#pin = pin;
+        this.currency = currency;
     }
 
-    calcAge() {
-        console.log(2037 - this.birthYear);
+    get movements() {
+        return this.#movements;
     }
 
-    get age() {
-        return 2037 - this.birthYear;
-    }
-
-    greet() {
-        console.log(`Hey ${this.firstName}`);
-    }
-
-    set fullName(name) {
-        if (name.includes(' ')) {
-            this._fullName = name;
-        }
-    }
-
-    get fullName() {
-        return this._fullName;
-    }
-
-    static hey() {
-        console.log('Hey there');
+    #approveLoan(val) {
+        return true;
     }
 }
 
-class StudentCL extends PersonCL {
-    constructor(fullName, birthYear, course) {
-        super(fullName, birthYear);
-        this.course = course;
-    }
-
-    introduce() {
-        console.log(`My name is ${this.fullName} and I study ${this.course}`);
-    }
-
-    calcAge() {
-        console.log(`I'm`);
-    }
-}
-
-const martha = new StudentCL('Martha Jones', 2012, 'CS');
-martha.introduce();
-martha.calcAge();
-
-const PersonProto = {
-    calcAge() {
-        console.log(2037 - this.birthYear);
-    },
-
-    init(firstName, birthYear) {
-        this.firstName = firstName;
-        this.birthYear = birthYear;
-    }
-}
-
-const steven = Object.create(PersonProto);
-
-const StudentProto = Object.create(PersonProto);
-StudentProto.init = function (firstName, birthYear, course) {
-    PersonProto.init.call(this, firstName, birthYear);
-    this.course = course;
-};
-StudentProto.introduce = function () {
-    console.log(`My name is ${this.firstName}`);
-}
-const jay = Object.create(StudentProto);
-jay.init('Jay', 2010, 'CS');
-jay.introduce();
+const acc1 = new Account('Jonas Schmedtmann', 'EUR', 1111);
+console.log(acc1);
+console.log(acc1.movements);
