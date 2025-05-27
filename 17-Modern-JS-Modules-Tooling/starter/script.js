0fedee4bed1b75decdf85f4c882db9aaa6acc10a
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2025-05-27 16:05:40 +0300

    Modules: await

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -11,9 +11,28 @@
 // console.log(ShoppingCart.totalPrice, ShoppingCart.tq);
 
 import Shipping from './shoppingCart.js';
+// Importing module
+console.log('Importing Module');
 
 console.log(Shipping);
-Shipping.function('apple', 4);
-Shipping.function('pizza', 2);
-Shipping.function('bread', 5);
+Shipping.add('apple', 4);
+Shipping.add('pizza', 2);
+Shipping.add('bread', 5);
 console.log(Shipping.cart);
+
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+
+const getLastPost = async function () {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+    return {title: data.at(-1), text: data.at(-1).body};
+}
+
+const lastPost = getLastPost();
+console.log(lastPost);
+//lastPost.then(data => console.log(data));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,5 +1,11 @@
 // Exporting Module
 console.log('Exporting Module');
+
+// Blocking code
+// console.log('Start fetching user data...');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('User data fetched');
+
 const shippingCost = 10;
 export const cart = [];
 
